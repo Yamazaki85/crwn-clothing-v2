e0d8d22458b1143e280151544f629ae5470a9fef
Lala Yamazaki <lalayamazaki@Lalas-MBP.attlocal.net>
2022-08-26 08:42:40 -0500

    created authentication alongside sign in form

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,6 +3,7 @@ import CategoryItem from "../category-item/category-item.component";
 import "./directory.style.scss";
 
 const Directory = ({ categories }) => {
+  console.log(categories);
   return (
     <div className="directory-container">
       {categories.map((category) => (
